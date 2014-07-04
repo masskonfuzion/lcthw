eef5ffc3706ff82b22fe0424ef4aa82df6536327
U-NAVTEQ\lherard <lherard@lherardBCT8FS1.ad.navteq.com>
2014-07-04 11:00:00 -0500

    Added exercise 11

---
+++ ex10/ex10.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	int i = 0;
+
+	// Go through each string in argv
+	// Why am I skipping argv[0]?
+	// (The answer is that argv[0] is the name of the program running)
+	for (i = 1; i < argc; i++) {
+		printf("arg %d: %s\n", i, argv[i]);
+	}
+
+	//Let's make our own array of strings
+	char *states[] = {"California", "Oregon", "Washington", "Texas"};
+
+	int num_states = 4;
+
+	for (i = 0; i < num_states; i++) {
+		printf("State %d: %s\n", i, states[i]);
+	}
+
+	return 0;
+}
